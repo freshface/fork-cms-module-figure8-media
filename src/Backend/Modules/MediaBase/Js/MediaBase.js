21b689c4146e27f8e8f4c1cd72e6a0a0714e6072
Frederik Heyninck <frederikheyninck@gmail.com>
2016-03-11 10:38:35 +0100

    ignore

---
+++ src/Backend/Modules/MediaBase/Js/MediaBase.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Interaction for the MediaBrands module
+ *
+ * @author Frederik Heyninck <frederik@figure8.be>
+ */
+jsBackend.media_base =
+{
+    // constructor
+    init: function()
+    {
+
+    }
+}
+
+$(jsBackend.media_base.init);
